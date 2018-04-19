f9c093fa66d33b542f2baadd016ae6250c6b12c2
RDNyel <al12542062@unadmexico.mx>
2018-04-18 20:04:14 -0500

    :+1:

--- js/main.js
+++ js/main.js
@@ -8,3 +8,20 @@ $(document).foundation({
 	}
 });
 
+$(document).ready(function() {
+	var nav=$(`.top-bar`);
+
+	$(window).scroll(function() {
+		var scroll =$(window).scrollTop();
+
+		if(scroll>=300){
+      nav.addClass(`active`);
+		}else{
+			nav.removeClass(`active`);
+
+		}
+	});
+});
+
+
+
