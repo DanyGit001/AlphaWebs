5087fe7e4a54bf6b599d90886868ef4f265f8fe4
RDNyel <al12542062@unadmexico.mx>
2018-05-08 19:16:05 -0500

    :+1:

--- js/main.js
+++ js/main.js
@@ -9,28 +9,23 @@ $(document).foundation({
 });
 
 $(document).ready(function() {
-	var nav=$(`.top-bar`);
+	var nav = $('.top-bar');
 
 	$(window).scroll(function() {
-		var scroll =$(window).scrollTop();
+		var scroll = $(window).scrollTop();
 
-		if(scroll>=300){
-      nav.addClass(`active`);
+		if(scroll >=300) {
+			nav.addClass('active');
 		} else {
-			nav.removeClass(`active`);
-
+			nav.removeClass('active');
 		}
 	});
 
-$(`body#index nav a:contains("Inicio")`).parent().addClass(`activo`);
-$(`body#nosotros nav a:contains("Nosotros")`).parent().addClass(`activo`);
-$(`body#servicios nav a:contains("Servicios")`).parent().addClass(`activo`);
-$(`body#equipo nav a:contains("Equipo de Trabajo")`).parent().addClass(`activo`);
-$(`body#contacto nav a:contains("Contacto")`).parent().addClass(`activo`);
 
-
-
+	$('body#index nav a:contains("Inicio")').parent().addClass('activo');
+	$('body#nosotros nav a:contains("Nosotros")').parent().addClass('activo');
+	$('body#servicios nav a:contains("Servicios")').parent().addClass('activo');
+	$('body#equipo nav a:contains("Equipo de Trabajo")').parent().addClass('activo');
+	$('body#contacto nav a:contains("Contacto")').parent().addClass('activo');
+	
 });
-
-
-
